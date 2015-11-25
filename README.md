9189b25c73db3cfd528a8b440c62b53b79d2fe53
Rosario Grosso <rosario@grosso.it>
2015-11-25 01:43:30 +0000

    updated Readme description

--- README.md
+++ README.md
@@ -1,28 +1,10 @@
 # ElasticSearch Vagrant Machine
 
-## BRANCH Master
 ## Installed Packages:
 
 - Ubuntu 14.04
 - Sun Java 8
-- ElasticSearch 2.0
-- ElastisSearch Plugins:
-
-
-
-
-
-
-
-
-
-
-## BRANCH 1.7
-## Installed Packages:
-
-- Ubuntu 14.04
-- Sun Java 8
-- ElasticSearch 1.7.x
+- ElasticSearch 2.1.x
 - ElastisSearch Plugins:
  - marvel
  - elasticsearch-HQ
@@ -36,19 +18,19 @@ Clone this repository in your local machine and run vagrant up:
     $>cd es-vagrant
     $>vagrant up
 
-Vagrant will start a new machine with elasticsearch 1.7 at 192.168.29.7
+Vagrant will start a new machine with elasticsearch 2.x at 192.168.29.200
 
 I suggest to add a DNS to your /etc/hosts file:
 
-    $>sudo echo "192.168.29.7 elasticsearch17.dev" >> /etc/hosts
+    $>sudo echo "192.168.29.200 elasticsearch.vagrant" >> /etc/hosts
 
 You can connect to ElasticSearch pointing your browser at
- - http://192.168.29.7:9200/
- - or http://elasticsearch17.dev:9200/ (if you are using the dns)
+ - http://192.168.29.200:9200/
+ - or http://elasticsearch.vagrant:9200/ (if you are using the dns)
 
 ## ElasticSearch Plugins
- - Marvel http://elasticsearch17.dev:9200/_plugin/marvel
- - ElasticSearch HQ http://elasticsearch17.dev:9200/_plugin/HQ/
- - kopf http://elasticsearch17.dev:9200/_plugin/kopf/
+ - Marvel http://elasticsearch.vagrant:9200/_plugin/marvel
+ - ElasticSearch HQ http://elasticsearch.vagrant:9200/_plugin/hq/
+
 
  Happy Hacking
--- provisioning.sh
+++ provisioning.sh
@@ -7,20 +7,23 @@ sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 
-# Install ElasticSearch 1.7.x (http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/setup-repositories.html)
+# Install ElasticSearch 2.x (http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/setup-repositories.html)
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 sudo apt-get update
 sudo apt-get install elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
+#starting from 2.0 ES is listening only on localhost by default. Add the IP
+cat "network.host: 192.168.29.200" >> /etc/elasticsearch/elasticsearch.yml
 # SysV init distributions - Ubuntu
 sudo /etc/init.d/elasticsearch start
 
 # Install ElasticSearch Plugins
-#cd /usr/share/elasticsearch
-#sudo bin/plugin --install elasticsearch/marvel/latest
-#sudo bin/plugin --install royrusso/elasticsearch-HQ
-#sudo bin/plugin --install lmenezes/elasticsearch-kopf/1
+cd /usr/share/elasticsearch
+sudo bin/plugin install license
+sudo bin/plugin install marvel-agent
+sudo bin/plugin install royrusso/elasticsearch-HQ
+
 
 # Install Python Tools
 sudo apt-get install build-essential autoconf flex bison libtool python-dev -y
